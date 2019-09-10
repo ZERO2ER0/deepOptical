d9999e39a5bb827b7400db3455e4491c1eb3e870
ChenLi <19976561@qq.com>
2019-09-10 10:55:49 +0800

    add optics.py

---
+++ optics.py
@@ -0,0 +1,9 @@
+##################################
+# optics with PyTorch
+##################################
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from numpy.fft import ifftshift
+import fractions
